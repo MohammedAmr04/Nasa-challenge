db2f565b540cd47737c6a46861e0b76def79d50d
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-10-02 23:44:10 +0300

    Add ChatBotPage Make Message component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,25 @@
 import GraphComponent from '@/components/KnowledgeGraph/GraphComponent'
+import SearchSemantic from '@/components/ui/SearchSemantic'
 
 function HomePage() {
   return (
-    <div className="grid grid-cols-5 pt-9">
-      <div className="max-w-md w-full  col-span-1 px-4  ">
-        <h1 className="text-2xl font-bold   text-gray-900 font-sans pb-7 ">
+    <div className="grid grid-cols-5 relative pt-9">
+      <div className="max-w-md w-full col-span-1 px-4  ">
+        <h1 className="text-2xl font-bold text-center   text-gray-900 font-sans  ">
           Knowledge Graph
         </h1>
+        <div>
+          <SearchSemantic />
+        </div>
       </div>
-      <div className="max-w-md w-full col-start-5 col-span-1 px-4  ">
+      <div className="col-span-3  h-screen overflow-y-auto">
+        <div className="p-4 space-y-4">
+          {Array.from({ length: 50 }).map((_, i) => (
+            <p key={i}>Content line {i + 1}</p>
+          ))}
+        </div>
+      </div>
+      <div className="max-w-md  w-full col-start-5  col-span-1 px-4  ">
         <h2 className="text-xl leading-2.5 text-gray-900 uppercase font-semibold font-sans pb-7 ">
           Interactive Graph
         </h2>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,3 +1,4 @@
+import ChatBotPage from '@/pages/ChatBotPage'
 import HomePage from '@/pages/HomePage'
 import LoginPage from '@/pages/LoginPage'
 import SignupPage from '@/pages/SignupPage'
@@ -5,6 +6,7 @@ import { createBrowserRouter } from 'react-router'
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
+  { path: '/chat', element: <ChatBotPage /> },
   { path: '/login', element: <LoginPage /> },
   { path: '/signup', element: <SignupPage /> },
 ])
